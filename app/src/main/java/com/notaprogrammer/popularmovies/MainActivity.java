06716b269d6ed343cb6e52e2c04c085babd09f3d
TheGratefulDev <ka.ling@live.com>
2018-10-02 19:45:56 -0500

    add error message textveiw for empty view

--- app/src/main/java/com/notaprogrammer/popularmovies/MainActivity.java
+++ app/src/main/java/com/notaprogrammer/popularmovies/MainActivity.java
@@ -1,18 +1,23 @@
 package com.notaprogrammer.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_empty_view) TextView emptyViewTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        TextView textView = findViewById(R.id.test);
-        textView.setText(BuildConfig.TheMovieDBAPIKey);
-
+    private void displayErrorMessage(){
+        emptyViewTv.setVisibility(View.VISIBLE);
     }
 }
