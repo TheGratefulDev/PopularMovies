b9bcc7622e7160a3274c47939c22f3075d8af8b4
TheGratefulDev <ka.ling@live.com>
2018-10-02 19:36:16 -0500

    add API/Secret key Properties

--- app/src/main/java/com/notaprogrammer/popularmovies/MainActivity.java
+++ app/src/main/java/com/notaprogrammer/popularmovies/MainActivity.java
@@ -2,6 +2,7 @@ package com.notaprogrammer.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.test);
+        textView.setText(BuildConfig.TheMovieDBAPIKey);
+
     }
 }
